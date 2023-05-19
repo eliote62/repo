1aa4cffdb36802336ad00842b2b8b11dc2661c03
eliote62 <bene@DESKTOP-U5FGT6U>
2023-05-19 22:24:15 +0200

    modifica funciones

--- S3_noabeliano.py
+++ S3_noabeliano.py
@@ -1,34 +1,61 @@
-#DEMOSTRACION mediante el metodo del CONTRAEJEMPLO del TEOREMA DE TEORIA DE GRUPOS:
-#dado un conjunto finito X con n elementos, el GRUPO SIMETRICO Sn con n>=3, cuyos elementos son las
-#aplicaciones biyectivas entre los elementos de X
+# DEMOSTRACION mediante el metodo del CONTRAEJEMPLO del TEOREMA DE TEORIA DE GRUPOS:
+# dado un conjunto finito X con n elementos, el GRUPO SIMETRICO Sn con n>=3, cuyos elementos son las
+# aplicaciones biyectivas entre los elementos de X
 
-X = [1, 2, 3] #tomamos un conjunto finito X con 3 elementos, que cumple la condicion del teorema n>=3
+# tomamos un conjunto finito X con 3 elementos, que cumple la condicion del teorema n>=3
+X = [1, 2, 3] 
 
-def f(x):               #definimos la funcion f, que es un elemento de S3 por ser aplicacion biyectiva
+def f(x):
+    """
+    definimos la funcion f, que es un elemento de S3 por ser aplicacion biyectiva.
+
+    args:
+        x: x es lo que sea
+
+    return:
+        retorna lo que sea
+    """
     if x == 1 :
-        resultado = 2
+        return  2
     elif x== 2 :
-        resultado = 3
+        return  3
     else :
-        resultado = 1
-    return resultado     
-def g(y):               #definimos la función g, otro elemento de S3.
+        return  1
+         
+
+def g(y):
+    """
+    definimos la función g, otro elemento de S3..
+
+    args:
+        y: y es lo que sea
+
+    return:
+        retorna lo que sea
+    """
     if y == 1 :
-        resultado = 2
+        return 2
     elif y == 2 :
-        resultado = 1
+        return 1
     else :
-        resultado = 3
-    return resultado
-print("introduce un elemento de X={1, 2, 3} = ")   
-elemento = int(input())                                 #introducimos por teclado uno de los elementos de X
+        return 3
 
-composicion_gf = g(f(elemento))                        #operamos los dos elementos de S3, f compuesta con g : g(f(x))
-print("f compuesta con g es g(f(elemento)) = " + str(composicion_gf))
-composicion_fg = f(g(elemento))
-print("g compuesta con f es f(g(elemento)) = " + str(composicion_fg))                                   #conmutamos la operacion, g compuesta con f : f(g(x))
+def abeliano(elemento):
+    # operamos los dos elementos de S3, f compuesta con g : g(f(x))
+    composicion_gf = g(f(elemento))
+    print("f compuesta con g es g(f(elemento)) = " + str(composicion_gf))
+    composicion_fg = f(g(elemento))
+    # conmutamos la operacion, g compuesta con f : f(g(x))
+    print("g compuesta con f es f(g(elemento)) = " + str(composicion_fg))
+    return composicion_gf == composicion_fg
 
-if composicion_gf == composicion_fg :                   #comprobamos si los elementos del conjunto S3 cumplen la propiedad conmutativa
-    print(" El conjunto S3 es ABELIANO")
-else :
-    print(" El conjunto S3 no es ABELIANO, ya que g(f(elemento) no es igual a f(g(elemento))")
+if __name__ == "__main__":
+    # introducimos por teclado uno de los elementos de X
+    print("introduce un elemento de X={1, 2, 3} = ")
+    elemento = int(input())
+
+    # comprobamos si los elementos del conjunto S3 cumplen la propiedad conmutativa
+    if  abeliano(elemento):
+        print(" El conjunto S3 es ABELIANO")
+    else :
+        print(" El conjunto S3 no es ABELIANO, ya que g(f(elemento) no es igual a f(g(elemento))")
